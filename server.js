e9f555d1f4c08acb28ee20818140e7d9db8f2550
kt-uchiha <kanitshrm09@gmail.com>
2017-02-11 17:22:51 +0530

    adding pages

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('server running');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('connected : %s sockets connected',connections.length);
+
+    socket.on('disconnect',function (data) { 
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected : %s sockets connected', connections.length);
+    });
+
+    socket.on('send message', function (data) {
+        console.log(data);
+        io.sockets.emit('new message', { msg: data });
+    });
+
+});
+ 
+
