208187f158cda66bcd5c3683a05929477fce929c
kt-uchiha <kanitshrm09@gmail.com>
2017-04-09 03:41:54 +0530

    More upgrades

--- public/controller.js
+++ public/controller.js
@@ -1,11 +1,19 @@
 var socket = io.connect();
 var nameg;
-
+let boo = false
+myapp.controller("ggcontroller" ,($scope) => {
+  $scope.clicked = (e) => {
+    if(boo == false)
+    e.preventDefault();
+  }
+})
 myapp.controller("ctrl1",function($scope,$location){
+
     $scope.login = function(path) {
     var name = document.getElementById('name');
     var email = document.getElementById('email');
     nameg = name.value;
+    boo = true;
     $location.path( path );
     }
 });
@@ -15,17 +23,19 @@ myapp.controller("ctrl2",['$scope',function($scope){
         socket.on('new message', function(data,name1){
         $scope.messages.push({"name" : name1 , "message" : data});
         $scope.$apply();
-
+        updatescroll()
   });
+
+    const updatescroll = ()=> {
+      var chat = document.getElementById('chat');
+      chat.scrollTop = chat.scrollHeight;
+    }
 
     $scope.myFunc = function(e) {
       e.preventDefault();
       var msg = document.getElementById('message');
       socket.emit('send message',msg.value,nameg);
       msg.value = '';
-
-
-
     };
 
 }]);
